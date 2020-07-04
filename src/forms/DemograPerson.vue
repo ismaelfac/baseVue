876c8d41f3e7ebd4a1db39f3645199d514e5df03
Ismael E. Lastre Alvarez <ismaelfac@gmail.com>
2020-07-03 19:44:24 -0500

    SideBar ok

--- src/forms/DemograPerson.vue
+++ src/forms/DemograPerson.vue
@@ -2,39 +2,63 @@
   <div>
     <validation-observer ref="observer" v-slot="{ handleSubmit }">
       <b-form @submit.stop.prevent="handleSubmit(onSubmit)">
-        
-        <validation-provider name="Name" :rules="{ required: true, min: 4 }" v-slot="validationContext">
-          <b-form-group id="names" label="Name" label-for="example-input-1">
-            <b-form-input
-              id="vname"
-              name="vname"
-              class="col-3"
-              v-model="form.name"
-              size="sm"
-              :state="getValidationState(validationContext)"
-              aria-describedby="input-1-live-feedback"
-            ></b-form-input>
+        <b-container fluid>
+            <b-row>
+              <b-col>
+                <validation-provider name="Name" :rules="{ required: true, min: 4 }" v-slot="validationContext">
+                  <b-input-group prepend="Tipo Dni" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                    <b-form-input
+                      id="vname"
+                      name="vname"
+                      class="col-12"
+                      v-model="form.name"
+                      size="sm"
+                      :state="getValidationState(validationContext)"
+                      aria-describedby="input-1-live-feedback"
+                    ></b-form-input>
 
-            <b-form-invalid-feedback id="input-1-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
-          </b-form-group>
-        </validation-provider>
-        <validation-provider name="Food" :rules="{ required: true }" v-slot="validationContext">
-          <b-form-group id="example-input-group-2" label="Food" label-for="example-input-2">
-            <b-form-select
-              id="vfood"
-              name="vfood"
-              class="col-3"
-              v-model="form.food"
-              :options="foods"
-              size="sm"
-              :state="getValidationState(validationContext)"
-              aria-describedby="input-2-live-feedback"
-            ></b-form-select>
+                    <b-form-invalid-feedback  id="input-1-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                  </b-input-group>
+                </validation-provider>
+              </b-col>
+              <b-col>
+                <validation-provider name="Food" :rules="{ required: true }" v-slot="validationContext">
+                  <b-input-group prepend="Dni" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                    <b-form-select
+                      id="vfood"
+                      name="vfood"
+                      class="col-12"
+                      v-model="form.food"
+                      :options="foods"
+                      size="sm"
+                      :state="getValidationState(validationContext)"
+                      aria-describedby="input-food"
+                    ></b-form-select>
 
-            <b-form-invalid-feedback id="input-2-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
-          </b-form-group>
-        </validation-provider>
-      
+                    <b-form-invalid-feedback id="input-food">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                  </b-input-group>
+                </validation-provider>
+              </b-col>
+              <b-col>
+                <validation-provider name="Option1" :rules="{ required: true }" v-slot="validationContext">
+                  <b-input-group prepend="Genero" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                    <b-form-select
+                      id="voption"
+                      name="voption"
+                      class="col-12"
+                      v-model="form.option1"
+                      :options="foods"
+                      size="sm"
+                      :state="getValidationState(validationContext)"
+                      aria-describedby="input-2-live-feedback"
+                    ></b-form-select>
+
+                    <b-form-invalid-feedback id="input-2-live-feedback">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                  </b-input-group>
+                </validation-provider>
+              </b-col>
+            </b-row>       
+        </b-container>    
         <validation-provider name="option" :rules="{ required: true }" v-slot="validationContext">
           <b-form-group id="optinGener" class="col-4" label="Option" label-for="example-input-2">
           <b-form-radio-group 
@@ -74,7 +98,8 @@ export default {
       form: {
         name: null,
         food: null,
-        option: null
+        option: null,
+        option1: null
       }
     };
   },
@@ -92,6 +117,7 @@ export default {
       this.form = {
         name: null,
         food: null,
+        option1: null,
         option: null
       };
 
--- src/forms/PersonalInformation.vue
+++ src/forms/PersonalInformation.vue
@@ -1,36 +1,155 @@
 <template>
     <div>
-        <b-form inline>
+        <validation-observer ref="observer" v-slot="{ handleSubmit }">
+        <b-form @submit.stop.prevent="handleSubmit(onSubmit)" inline>
+            <b-container fluid>
+              <b-row>
+                <b-col md="4" lg="3" sm="6">
+                  <validation-provider name="Dni" :rules="{ required: true, min: 4 }" v-slot="validationContext">
+                    <b-input-group prepend="Dni" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                      <b-form-input
+                        id="dni"
+                        name="dni"
+                        v-model="form.dni"
+                        size="sm"
+                        :state="getValidationState(validationContext)"
+                        aria-describedby="input-dni"
+                      ></b-form-input>
 
-            <label class="sr-only" for="inline-form-input-name">Name</label>
-            <b-input
-                id="inline-form-input-name"
-                class="mb-2 mr-sm-2 mb-sm-0"
-                placeholder="Jane Doe"
-            ></b-input>
+                      <b-form-invalid-feedback  id="input-dni">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                    </b-input-group>
+                  </validation-provider>
+                </b-col>
+                <b-col md="4" lg="3" sm="6">
+                  <validation-provider name="Tipo Dni" :rules="{ required: true }" v-slot="validationContext">
+                    <b-input-group prepend="Tipo Dni" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                      <b-form-select
+                        id="type_dni"
+                        name="type_dni"
+                        v-model="form.type_dni"
+                        :options="options_dni"
+                        size="sm"
+                        :state="getValidationState(validationContext)"
+                        aria-describedby="input-type-dni"
+                      ></b-form-select>
 
-            <label class="sr-only" for="inline-form-input-username">Username</label>
-            <b-input-group prepend="Usuario" class="mb-2 mr-sm-2 mb-sm-0">
-            <b-input id="inline-form-input-username" placeholder="Username"></b-input>
-            </b-input-group>
+                      <b-form-invalid-feedback id="input-type-dni">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                    </b-input-group>
+                  </validation-provider>
+                </b-col>
+                <b-col md="4" lg="3" sm="6">
+                  <validation-provider name="Nombres" :rules="{ required: true, min: 4 }" v-slot="validationContext">
+                    <b-input-group prepend="Nombres" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                      <b-form-input
+                        id="names"
+                        name="names"
+                        v-model="form.names"
+                        size="sm"
+                        :state="getValidationState(validationContext)"
+                        aria-describedby="input-names"
+                      ></b-form-input>
 
-            <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+                      <b-form-invalid-feedback  id="input-names">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                    </b-input-group>
+                  </validation-provider>
+                </b-col>
+                <b-col md="4" lg="3" sm="6">
+                  <validation-provider name="Apellidos" :rules="{ required: true, min: 4 }" v-slot="validationContext">
+                    <b-input-group prepend="Apellidos" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                      <b-form-input
+                        id="surnames"
+                        name="surnames"
+                        v-model="form.surnames"
+                        size="sm"
+                        :state="getValidationState(validationContext)"
+                        aria-describedby="input-surnames"
+                      ></b-form-input>
 
-            <b-button variant="primary">Save</b-button>
+                      <b-form-invalid-feedback  id="input-surnames">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                    </b-input-group>
+                  </validation-provider>
+                </b-col>
+                <b-col md="4" lg="3" sm="6">
+                  <validation-provider name="gender" :rules="{ required: true }" v-slot="validationContext">
+                    <b-input-group prepend="Genero" size="sm" class="mb-2 mr-sm-2 mb-sm-2">
+                      <b-form-radio-group
+                        id="gender"
+                        v-model="form.gender"
+                        :options="option_gender"
+                        size="sm"
+                        buttons
+                        name="gender"
+                        aria-describedby="input-gender"
+                      ></b-form-radio-group>
+                      <b-form-invalid-feedback id="input-gender">{{ validationContext.errors[0] }}</b-form-invalid-feedback>
+                    </b-input-group>
+                  </validation-provider>
+                </b-col>
+                <b-col>
+                  <hr />
+                  <b-button type="submit" variant="primary">Submit</b-button>
+                  <b-button class="ml-2" @click="resetForm()">Reset</b-button>
+                </b-col>
+              </b-row>       
+          </b-container>
+            
         </b-form>
+        </validation-observer>
     </div>
 </template>
 <script>
 export default {
     name: 'PersonalInformation',
     data() {
-        return {
-            form: {
-                id: '',
-                names: '',
-                surnames: ''
-            }
-        }
+    return {
+      options_dni: [
+        { value: null, text: "Tipo de Dni" },
+        { value: "CC", text: "Cedula de Ciudadania" },
+        { value: "TI", text: "Tarjeta de Identidad" },
+        { value: "PP", text: "Pasaporte" }
+      ],
+      options: [
+          { text: 'First radio', value: 'first' },
+          { text: 'Second radio', value: 'second' },
+          { text: 'Third radio', value: 'third' }
+      ],
+      option_gender: [
+          { value: 'male', 'text': "Masculino" },
+          { value: 'female', 'text': "Femenino" }
+      ],
+      form: {
+        dni: null,
+        type_dni: null,
+        names: null,
+        surnames: null,
+        gender: null
+      }
+    };
+  },
+  computed: {
+    state() {
+      return Boolean(this.value)
     }
-}
+  },
+  methods: {
+    getValidationState({ dirty, validated, valid = null }) {
+        console.log('dyrty: '+dirty+ ' Validated: '+ validated);
+      return dirty || validated ? valid : null;
+    },
+    resetForm() {
+      this.form = {
+        name: null,
+        food: null,
+        option: null
+      };
+
+      this.$nextTick(() => {
+        this.$refs.observer.reset();
+      });
+    },
+    onSubmit() {
+      alert("Form submitted!");
+    }
+  }
+};
 </script>
